05428b775cf97d4c3ecf71ced9baa2698f980731
VinaySarilla <vinayknown@gmail.com>
2019-07-26 06:32:14 +0000

    jais
    aa

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start editing to see some magic a!</h2>
     </div>
   );
 }
